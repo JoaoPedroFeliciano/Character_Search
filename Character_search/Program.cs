1f0dca99aaa1a23845eeb7ad650b1738db0684d2
João Pedro <joao.pedro.guatapara@gmail.com>
2024-09-03 14:35:13 -0300

    Add project files.

---
+++ Character_search/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma frase:");
+        string a = Console.ReadLine();
+
+        int x = 0;
+
+        foreach (char c in a)
+        {
+
+            if (c == 'a' || c == 'A')
+            {
+                x++;
+            }
+        }
+
+        if (x == 0)
+        {
+            Console.WriteLine("A letra 'a' não aparece na frase.");
+        }
+        else
+        {
+            Console.WriteLine($"A letra 'a' aparece {x} vezes na frase.");
+        }
+    }
+}
